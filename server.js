cfdb9136e306fad7a97e7aa3f5fb335456c9f83b
rafael-men <rafaelgoncalves1822@gmail.com>
2024-04-30 13:57:36 -0300

    final touches

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const App = express()
+const app = express();
+const server = require("http").createServer(app);
 
-const server = require('http').createServer(App)
+const io = require("socket.io")(server);
 
-const io = require('socket.io')(server)
+app.use(express.static(path.join(__dirname+"/public")));
 
-App.use(express.static(path.join(__dirname + '/public')))
+io.on("connection", function(socket){
+	socket.on("newuser",function(username){
+		socket.broadcast.emit("update", username + " joined the conversation");
+	});
+	socket.on("exituser",function(username){
+		socket.broadcast.emit("update", username + " left the conversation");
+	});
+	socket.on("chat",function(message){
+		socket.broadcast.emit("chat", message);
+	});
+});
 
-io.on('connection',function(socket) {
-    socket.on('newuser',function(username) {
-        socket.broadcast.emit('update', username + ' joined the conversation')
-    })
-    socket.on('exituser',function(username) {
-        socket.broadcast.emit('update', username + ' left the conversation')
-    })
-    socket.on('chat',function(message) {
-        socket.broadcast.emit('chat', message)
-    })
-})
-server.listen(3001)
+server.listen(5000);
